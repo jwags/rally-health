d0ccec08e4fa83a5302043a8e94031b72f3c85c8
John Martin <curmudgeon@rallydev.com>
2013-09-06 12:24:42 -0700

    add iteration selector

--- src/javascript/z_app.js
+++ src/javascript/z_app.js
@@ -2,33 +2,83 @@ Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
     logger: new Rally.technicalservices.logger(),
-    items: [ { itemId:'grid_box' } ],
+    items: [ { xtype:'container',itemId:'selector_box', padding: 5, layout: { type:'hbox'} }, { xtype:'container', itemId:'grid_box', padding: 5 } ],
     launch: function() {
+        this._getProjects();
+        this._addIterationSelector();
+    },
+    _getProjects: function() {
         var me = this;
-        this.logger.log(this,this.getContext());
+        this.project_array = null;
         var selected_project_oid = this.getContext().getProject().ObjectID;
-        this.logger.log(selected_project_oid);
         
         Ext.create('Rally.data.WsapiDataStore',{
             model:'Project',
             autoLoad: true,
             listeners: {
                 load: function(store,projects) {
-                    this.logger.log(this,projects);
                     var ts_project_hash = this._makeTSProjectHash(projects);
                     var ts_project_array = Rally.technicalservices.util.Utilities.structureProjects(ts_project_hash,true);
-                    this.logger.log(this,"project array",ts_project_array);
+                    var ts_selected_project = Rally.technicalservices.util.Utilities.getProjectById(ts_project_array,selected_project_oid);
                     
-                    var ts_selected_hash = Rally.technicalservices.util.Utilities.getProjectById(ts_project_array,selected_project_oid);
-                    this.logger.log(this,"selected",ts_selected_hash);
-                    
-                    var grid_array = Rally.technicalservices.util.Utilities.hashToOrderedArray(ts_selected_hash.getData(true),"children");
-                    this._makeGrid(grid_array);
+                    this.project_array = Rally.technicalservices.util.Utilities.hashToOrderedArray(ts_selected_project.getData(true),"children");
+                    this._processData();
                 },
                 scope: this
             }
         });
         
+    },
+    _addIterationSelector: function() {
+        this.iteration_selector = this.down("#selector_box").add({
+            xtype:'rallyiterationcombobox',
+            listeners: {
+                ready: function(cb){
+                    this.logger.log(this,"ready");
+                    this._updateIterationDisplay(cb);
+                },
+                change: function(cb,new_value,old_value){
+                    this.logger.log(this,"change");
+                    this._updateIterationDisplay(cb);
+                },
+                scope: this
+            }
+        });
+        this.down('#selector_box').add({
+            xtype:'container',
+            itemId: 'iteration_range_box',
+            tpl: [
+                "{start_date} - {end_date}",
+                '<tpl if="day_counter &gt; -1">',
+                "<br/>This is day {day_counter} of the iteration",
+                '</tpl>'
+            ]
+        });
+    },
+    _updateIterationDisplay: function(combobox) {
+        var timebox = combobox.getRecord();
+        var start_date = timebox.get(combobox.getStartDateField());
+        var end_date = timebox.get(combobox.getEndDateField());
+        var day_counter = -1;
+        var today = new Date();
+        if ( today >= start_date && today <= end_date ) {
+            day_counter = Rally.util.DateTime.getDifference(today,start_date,"day");
+        }
+        var formatted_start_date = Rally.util.DateTime.formatWithNoYearWithDefault(start_date);
+        var formatted_end_date = Rally.util.DateTime.formatWithNoYearWithDefault(end_date);
+        
+        this.down('#iteration_range_box').update({
+            start_date:formatted_start_date,
+            end_date:formatted_end_date,
+            day_counter:day_counter
+        });
+        this.logger.log(this,start_date,end_date);
+    },
+    _processData:function() {
+        //
+        if ( this.project_array ) {
+            this._makeGrid(this.project_array);
+        }
     },
     // given a set of Rally project objects, turn them into TS projects
     // the key of the hash is the project's ObjectID
@@ -59,6 +109,7 @@ Ext.define('CustomApp', {
         var store = Ext.create('Rally.technicalservices.ProjectStore',{
             data: projects
         });
+        if ( this.grid ) { this.grid.destroy(); }
         
         this.grid = Ext.create('Rally.ui.grid.Grid',{
             store: store,
